365b88fb5644fb4a2dbb8cef1ff80b3ab2dd4f2e
Deepesh Kansotia <97501085+deepeshkans@users.noreply.github.com>
2023-10-01 21:56:15 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,32 @@
+let questions=[
+    {
+        numb:1,
+        question:"What is the capital of France?",
+        answer:"D) Paris",
+        options:["A) Madrid","B) London","C) Rome","D) Paris"]
+    },
+    {
+        numb:2,
+        question:"Which Planet is known as the Red Planet",
+        answer:"B) Mars",
+        options:["A) Jupiter","B) Mars","C) Venus","D) Saturn"]
+    },
+    {
+        numb:3,
+        question:"Who wrote the play Romeo and Juliet?",
+        answer:"B) William Shakespeare",
+        options:["A) Charles Dickens","B) William Shakespeare", "C) Jane Austen","D) Mark Twain"]
+    },
+    {
+        numb:4,
+        question:"What is the chemical symbol for gold?",
+        answer:"C) Au",
+        options:["A) Go","B) Ag","C) Au","D) Fe"]
+    },  
+    {
+        numb:5,
+        question:"Which gas do plants absorb from the atmosphere during photosynthesis?",
+        answer:"B) Carbon Dioxide",
+        options:["A) Oxygen","B) Carbon Dioxide","C) Nitrogen","D) Hydrogen"]
+    }
+];
